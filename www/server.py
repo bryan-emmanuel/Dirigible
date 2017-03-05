a2d1e81e999ec3441e37189b398752a00d946aa8
Bryan Emmanuel <bemmanuel@meetme.com>
2017-03-05 14:35:51 -0500

    update server path

--- www/server.py
+++ www/server.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask('Dirigible')
 VIDEO_EXT = '(\.mp4$)'
-ROOT = os.path.dirname(os.path.abspath(__file__)) + "../www"
+ROOT = os.path.dirname(os.path.abspath(__file__)) + "/../www"
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
