42800fd3914b79e87ac929645a679600d19fcea5
Bryan Emmanuel <bemmanuel@meetme.com>
2017-03-17 12:38:58 -0400

    update buffer size for tomcat

--- web/src/main/java/com/piusvelte/dirigible/web/DirigibleServlet.java
+++ web/src/main/java/com/piusvelte/dirigible/web/DirigibleServlet.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class DirigibleServlet extends HttpServlet {
 
-    private static final int BUFFER_SIZE = 256 * 1024;
+    private static final int BUFFER_SIZE = 128 * 1024;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -57,21 +57,9 @@ public class DirigibleServlet extends HttpServlet {
 
                 writer.write("]}");
             } else if (realPath.endsWith(".jpg")) {
-                String param = req.getParameter("test");
-
-                if (param == null || param.length() == 0) {
-                    writeFile(resp, file, "image/jpeg");
-                } else {
-                    test(resp, file, param);
-                }
+                writeFile(resp, file, "image/jpeg");
             } else if (realPath.endsWith(".mp4")) {
-                String param = req.getParameter("test");
-
-                if (param == null || param.length() == 0) {
-                    writeFile(resp, file, "video/mp4");
-                } else {
-                    test(resp, file, param);
-                }
+                writeFile(resp, file, "video/mp4");
             } else {
                 writeEmptyData(resp);
             }
@@ -85,48 +73,14 @@ public class DirigibleServlet extends HttpServlet {
         response.getWriter().write("{\"data\":[]}");
     }
 
-    private void test(HttpServletResponse response, File file, String test) throws IOException {
-        response.setContentType("text/html");
-        response.getWriter().write("<html><body><p>");
-        response.getWriter().write("default buffer size: ");
-        response.getWriter().write(String.valueOf(response.getBufferSize()));
-        response.getWriter().write("</p>");
-
-        if ("read".equals(test)) {
-            long start = System.currentTimeMillis();
-            long totalRead = 0;
-            InputStream in = null;
-
-            try {
-                in = new FileInputStream(file);
-
-                byte[] buffer = new byte[BUFFER_SIZE];
-
-                for (int read; (read = in.read(buffer)) >= 0; ) {
-                    totalRead += read;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                closeQuietly(in);
-            }
-
-            if (totalRead > 0) {
-                response.getWriter().write("<p>read: ");
-                response.getWriter().write(String.valueOf(totalRead / ((System.currentTimeMillis() - start) * 1000.f)));
-                response.getWriter().write("m/s");
-            } else {
-                response.getWriter().write("no bytes read");
-            }
-        } else {
-            response.getWriter().write("unknown test: ");
-            response.getWriter().write(test);
-        }
-    }
-
     private void writeFile(HttpServletResponse response, File file, String mimeType) {
         response.setContentType(mimeType);
-        response.setBufferSize(BUFFER_SIZE);
+
+        if (file.length() < BUFFER_SIZE) {
+            response.setBufferSize((int) (512 * (file.length() / 512.f)));
+        } else {
+            response.setBufferSize(BUFFER_SIZE);
+        }
 
         InputStream in = null;
         OutputStream out = null;
@@ -135,11 +89,10 @@ public class DirigibleServlet extends HttpServlet {
             in = new FileInputStream(file);
             out = response.getOutputStream();
 
-            byte[] buffer = new byte[BUFFER_SIZE];
-            int read;
+            byte[] buffer = new byte[response.getBufferSize()];
 
-            while ((read = in.read(buffer)) >= 0) {
-                out.write(buffer, 0, read);
+            while (in.read(buffer) >= 0) {
+                out.write(buffer);
             }
         } catch (IOException e) {
             e.printStackTrace();
